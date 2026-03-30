b9282a856c293d9aaf6e16c6b6abcd0b69c6b1e0
AlecCode97-star <alessandro1492@live.it>
2026-03-30 15:04:49 +0200

    Changes AnimaleMapper.java workspace.xml

---
+++ animali/animali/src/main/java/it/adozioni/animali/Mapper/AnimaleMapper.java
@@ -0,0 +1,21 @@
+package it.adozioni.animali.Mapper;
+import it.adozioni.animali.Model.Animale;
+import it.adozioni.animali.Dto.AnimaleDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimaleMapper extends AbstractConverter<Animale,AnimaleDto> {
+    final private ModelMapper mapper = new ModelMapper();
+
+
+    @Override
+    public Animale toEntity(AnimaleDto dto) {
+        return mapper.map(dto,Animale.class);
+    }
+
+    @Override
+    public AnimaleDto toDTO(Animale entity) {
+        return mapper.map(entity,AnimaleDto.class);
+    }
+}
