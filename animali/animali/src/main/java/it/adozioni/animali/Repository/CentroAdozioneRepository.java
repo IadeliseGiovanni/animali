dc64f597c39fc75ed3c295f162ce62ae27b99c54
Giovanni Iadelise <iadelisegiovanni2000@gmail.com>
2026-03-30 14:47:02 +0200

    Changes CentroAdozioneRepository.java

--- animali/animali/src/main/java/it/adozioni/animali/Repository/CentroAdozioneRepository.java
+++ animali/animali/src/main/java/it/adozioni/animali/Repository/CentroAdozioneRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CentroAdozioneRepository extends JpaRepository<CentroAdozione, Long> {
-    // Qui non devi scrivere nulla, fa tutto Spring Boot!
+
 }
