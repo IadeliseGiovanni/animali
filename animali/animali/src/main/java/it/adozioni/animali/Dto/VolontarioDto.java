0ecbf4303156d304f48085c73d43d6ab50c04841
Barto080395 <bartolomeobraccio95@live.com>
2026-03-31 10:32:05 +0200

    costr

--- animali/animali/src/main/java/it/adozioni/animali/Dto/VolontarioDto.java
+++ animali/animali/src/main/java/it/adozioni/animali/Dto/VolontarioDto.java
@@ -1,5 +1,6 @@
 package it.adozioni.animali.Dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class VolontarioDto {
 
     private Integer id;
