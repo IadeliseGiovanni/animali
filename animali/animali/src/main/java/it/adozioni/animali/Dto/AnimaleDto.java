57a16fc5d6904e44328ed9df9dd13fc90f109e8d
AlecCode97-star <alessandro1492@live.it>
2026-03-31 10:33:29 +0200

    Changes AnimaleDto.java

--- animali/animali/src/main/java/it/adozioni/animali/Dto/AnimaleDto.java
+++ animali/animali/src/main/java/it/adozioni/animali/Dto/AnimaleDto.java
@@ -1,9 +1,14 @@
 package it.adozioni.animali.Dto;
 
+import it.adozioni.animali.Model.Adottante;
+import it.adozioni.animali.Model.CentroAdozione;
+import it.adozioni.animali.Model.VisitaMedica;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -16,6 +21,9 @@ public class AnimaleDto {
     private String razza;
     private int eta;
     boolean isAdottato;
+    private List<VisitaMedica> visiteMediche;
+    private Adottante adottante;
+    private CentroAdozione centroAdozione;
 
 
 }
